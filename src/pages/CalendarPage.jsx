96579ed15588e09d0b0b9653019c4a422cdffe07
olajimmy <overdedicated9@gmail.com>
2025-01-07 10:30:46 -0500

    getting data from my database, hurray

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,4 +1,51 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function CalendarPage() {
-  return <>CalendarPage</>;
+  const [entries, setEntries] = useState([]);
+  const LOCAL_URL = `http://localhost:5050`;
+
+  const getEntries = async () => {
+    console.log(`in getEntries`);
+    //fetch calendar entries from the back end
+    //aslo known as the api that i an creating
+    //this endpoint is:
+    // /api/calendar
+
+    try {
+      const response = await axios.get(`${LOCAL_URL}/api/calendar`);
+      console.log(response.data);
+      setEntries(response.data);
+    } catch (err) {
+      console.err(err);
+    }
+  };
+
+  useEffect(() => {
+    getEntries();
+  }, []);
+
+  const loading = () => {
+    return <h3>there dont seem to be an entries yet</h3>;
+  };
+  const loaded = () => {
+    <ul style={{ listStyleType: "none" }}>
+      {entries.map((entry) => {
+        return (
+          <li>
+            {entry.startDate}: {entry.label}
+          </li>
+        );
+      })}
+    </ul>;
+  };
+
+  return (
+    <>
+      <h2>Calendar Entries</h2>
+
+      {entries.length ? loaded() : loading()}
+    </>
+  );
 }
 export default CalendarPage;
--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,4 +1,63 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function TodoPage() {
-  return <>todo page</>;
+  //
+  const [entries, setEntries] = useState([]);
+  const LOCAL_URL = `http://localhost:5050`;
+
+  const getEntries = async () => {
+    console.log(`in getEntries`);
+    //fetch calendar entries from the back end
+    //aslo known as the api that i an creating
+    //this endpoint is:
+    // /api/todo
+
+    try {
+      const response = await axios.get(`${LOCAL_URL}/api/todo`);
+      console.log(response.data);
+      setEntries(response.data);
+    } catch (err) {
+      console.err(err);
+    }
+  };
+
+  //
+  useEffect(() => {
+    getEntries();
+  }, []);
+
+  const loading = () => {
+    return <h3>there dont seem to be an entries yet</h3>;
+  };
+  //
+  const loaded = () => {
+    <ul
+      style={{
+        listStyleType: "none",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {entries.map((entry) => {
+        return (
+          <li style={{ width: "80%" }}>
+            {entry.due}: {entry.completed ? <>DONE</> : <>_______</>}:
+            {entry.text}
+          </li>
+        );
+      })}
+    </ul>;
+  };
+
+  return (
+    <>
+      <h2>todo Entries</h2>
+
+      {entries.length ? loaded() : loading()}
+    </>
+  );
 }
+//
+
 export default TodoPage;
